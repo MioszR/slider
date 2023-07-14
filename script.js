1f791911a5866885ceb5a15f98fd44ebc2b320c9
MioszR <99885857+MioszR@users.noreply.github.com>
2023-07-14 14:56:18 +0200

    Add script

---
+++ script.js
@@ -0,0 +1,53 @@
+let activeSlideNumber = 1;
+let leftArrow = document.querySelector('.arrow-left');
+let rightArrow = document.querySelector('.arrow-right');
+
+let dot1 = document.querySelector('#dot1');
+let dot2 = document.querySelector('#dot2');
+let dot3 =document.querySelector('#dot3');
+
+let slide1 = document.querySelector('#slide1');
+let slide2 = document.querySelector('#slide2');
+let slide3 = document.querySelector('#slide3');
+
+
+let hideActiveSlide = () => {
+    let activeElement = document.querySelector('.active');
+    activeElement.classList.remove('active');
+}
+
+let showSlide = (slideNumber) => {
+    hideActiveSlide();
+    document.querySelector('#slide' + slideNumber).classList.add('active');
+}
+
+let showNextSlide = () => {
+    activeSlideNumber = activeSlideNumber +1;
+    if (activeSlideNumber > 3) {
+        activeSlideNumber = 1;
+        showSlide(activeSlideNumber);
+    } else {
+        showSlide(activeSlideNumber);
+    }
+}
+
+let showPreviousSlide = () => {
+    activeSlideNumber = activeSlideNumber - 1;
+    if (activeSlideNumber < 1) {
+        activeSlideNumber = 3;
+        showSlide(activeSlideNumber);
+    } else {
+        showSlide(activeSlideNumber );
+    }
+}
+
+for (let i = 1; i <= 3; i++) {
+    let showSlideI = () => {
+        showSlide(i);
+        activeSlideNumber = i;
+    }
+    document.querySelector('#dot' + i).addEventListener('click', showSlideI);
+}
+
+leftArrow.addEventListener('click', showPreviousSlide);
+rightArrow.addEventListener('click', showNextSlide);
